7db8711d72d3d1c913f7658eb1ed050e3a36691b
PatokaDafi <167584750+PatokaDafi@users.noreply.github.com>
2025-05-10 19:48:06 +0300

    Update1.1
    change the pick

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import toyotaLogo from "./png-transparent-toyota-gazoo-racing-hd-logo.png";
+import toyotaLogo from "./grlogo.png";
 
 const Landing = () => {
   return (
